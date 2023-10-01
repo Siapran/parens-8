b21eecf560223b0e18a996bd332467663a731b7f
twomice <siapran.candoris@gmail.com>
2023-10-01 15:21:12 -0400

    add builtin operators.

---
+++ parens-8/builtin_operators.lua
@@ -0,0 +1,29 @@
+function make_builtin(name, fn)
+	builtin[name] = function(exp, env, ev) return fn(ev, exp, env) end
+end
+
+make_builtin("neg", function(ev) return -ev(2) end)
+make_builtin("+", function(ev) return ev(2)+ev(3) end)
+make_builtin("-", function(ev) return ev(2)-ev(3) end)
+make_builtin("*", function(ev) return ev(2)*ev(3) end)
+make_builtin("/", function(ev) return ev(2)/ev(3) end)
+make_builtin("\\", function(ev) return ev(2)\ev(3) end)
+make_builtin("%", function(ev) return ev(2)%ev(3) end)
+make_builtin("^", function(ev) return ev(2)^ev(3) end)
+
+make_builtin("<", function(ev) return ev(2)<ev(3) end)
+make_builtin(">", function(ev) return ev(2)>ev(3) end)
+make_builtin("==", function(ev) return ev(2)==ev(3) end)
+make_builtin("~=", function(ev) return ev(2)~=ev(3) end)
+
+make_builtin("..", function(ev) return ev(2)..ev(3) end)
+
+make_builtin("or", function(ev) return ev(2) or ev(3) end)
+make_builtin("and", function(ev) return ev(2) and ev(3) end)
+make_builtin("not", function(ev) return not ev(2) end)
+
+make_builtin("#", function(ev) return #ev(2) end)
+make_builtin("[]", function(ev, exp)
+	if (exp[4]) ev(2)[ev(3)] = ev(4)
+	return ev(2)[ev(3)]
+end)
