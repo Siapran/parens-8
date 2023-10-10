43854211df45b8be4043a0fe09c96afca98d87db
twomice <siapran.candoris@gmail.com>
2023-10-10 00:39:02 -0400

    update minify.

--- rom-utils/minify.lua
+++ rom-utils/minify.lua
@@ -1,18 +1,25 @@
 function minify(str)
-	local function minify_(acc, str, strip)
-		if (str == "") return acc
-		local found, c = find(str, function(x)
-				return not any' \n\t'(x)
-			end)
-		local boundary = any'\'"()'(c)
-		local space = (boundary or strip) and "" or " "
-		if (found > 1) return minify_(acc .. space, sub(str, found), boundary)
-		if any'\'"'(c) then
-			local close = find(sub(str, 2), any(c))
-			return minify_(acc .. sub(str, 1, close + 1),
-				           sub(str, close + 2), true)
+	local function any(matches)
+		return function(v)
+			for m in all(matches) do
+				if (v == m) return true
+			end
 		end
-		return minify_(acc .. c, sub(str, 2), boundary)
 	end
-	return minify_("", str, true)
+
+	local function minify_(acc, off)
+		_ppos += off or 1
+		local last = _pstr[_ppos - 1]
+		local space = consume(' \n\t', true)
+		space = any'\'"()'(last)
+			and "" or sub(space, 1, 1)
+		local c = _pstr[_ppos]
+		if (not c) return acc
+		if (c == '(') return minify_(minify_(acc .. c))
+		if (c == ')') return acc .. c
+		if (any'\'"'(c)) _ppos += 1 return minify_(acc .. c .. consume(c) .. c)
+		return minify_(acc .. space .. consume' \n\t()', 0)
+	end
+	_pstr, _ppos = str, 0
+	return minify_("")
 end
