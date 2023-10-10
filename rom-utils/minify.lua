7d3f3f1d4392643c5e6e4e27bb8982ad3e6209d3
twomice <siapran.candoris@gmail.com>
2023-10-10 01:00:55 -0400

    simplify minifier.

--- rom-utils/minify.lua
+++ rom-utils/minify.lua
@@ -1,25 +1,15 @@
 function minify(str)
-	local function any(matches)
-		return function(v)
-			for m in all(matches) do
-				if (v == m) return true
-			end
-		end
-	end
-
-	local function minify_(acc, off)
+	local function minify_(acc, delim, off)
 		_ppos += off or 1
-		local last = _pstr[_ppos - 1]
 		local space = consume(' \n\t', true)
-		space = any'\'"()'(last)
-			and "" or sub(space, 1, 1)
+		space = delim and "" or sub(space, 1, 1)
 		local c = _pstr[_ppos]
 		if (not c) return acc
-		if (c == '(') return minify_(minify_(acc .. c))
+		if (c == '(') return minify_(minify_(acc .. c, true), true)
 		if (c == ')') return acc .. c
-		if (any'\'"'(c)) _ppos += 1 return minify_(acc .. c .. consume(c) .. c)
-		return minify_(acc .. space .. consume' \n\t()', 0)
+		if (c == '"' or c == "'") _ppos += 1 return minify_(acc .. c .. consume(c) .. c, true)
+		return minify_(acc .. space .. consume' \n\t()\'"', false, 0)
 	end
 	_pstr, _ppos = str, 0
-	return minify_("")
+	return minify_("", true)
 end
