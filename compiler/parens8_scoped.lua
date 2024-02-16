ef3d4984912e7b130145940424403f8e527b054a
twomice <siapran.candoris@gmail.com>
2024-02-16 00:37:06 -0500

    optimize token usage.

--- compiler/parens8_scoped.lua
+++ compiler/parens8_scoped.lua
@@ -41,10 +41,12 @@ function eval(exp, lookup)
 	end
 	if (type(exp) == "number") return function() return exp end
 
-	if exp[1] == "fn" then
+	local exp1, exp2, exp3 = unpack(exp)
+
+	if exp1 == "fn" then
 		local locals, captures, key = {}, {}, {}
-		for i,v in ipairs(exp[2]) do locals[v] = i end
-		local compiled = eval(exp[3], function(name)
+		for i,v in ipairs(exp2) do locals[v] = i end
+		local compiled = eval(exp3, function(name)
 			local idx, where = locals[name]
 			if idx then return idx, false end
 			idx, where = lookup(name)
@@ -63,7 +65,7 @@ function eval(exp, lookup)
 		end
 	end
 
-	local n, op, compiled = #exp, builtin[exp[1]], {}
+	local n, op, compiled = #exp, builtin[exp1], {}
 	for term in all(exp) do add(compiled, eval(term, lookup)) end
 	if (op) return op(exp, lookup, unpack(compiled, 2))
 
@@ -83,7 +85,7 @@ function builtin:set(lookup, _, a2)
 		and function(frame, upvals) upvals[where][idx] = a2(frame, upvals) end
 		or function(frame, upvals) frame[idx] = a2(frame, upvals) end
 end
-function builtin:when(lookup, a1, a2, a3)
+function builtin:when(_, a1, a2, a3)
 	return function(...)
 		if (a1(...)) return a2(...)
 		if (a3) return a3(...)
