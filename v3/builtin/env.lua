5c7a317c8c5e31176284f72ac5659283a4846396
twomice <siapran.candoris@gmail.com>
2024-02-28 03:01:35 -0500

    improve env builtin.

--- v3/builtin/env.lua
+++ v3/builtin/env.lua
@@ -1,34 +1,33 @@
 -- local _ENV = self; x = 42
 -- (env self (set x 42))
-def_builtin("env", function(frame, a1, a2)
-	-- frame[1][1] = a1()
-	return a2(setmetatable(
-		{setmetatable({a1(frame)}, {__index = frame[1]})},
-		{__index = frame, __newindex = frame}))
-end)
+function builtin.env(...)
+	local key, mapped, body = parens8[[
+		(fn (exp) ((fn (key lookup exp1 exp2) (id
+			key (compile exp1 lookup)
+			(compile exp2 (fn (name)
+				((fn (idx where) (id
+					idx (when (tonum (tostring idx)) where key)
+				)) (lookup name))
+			))
+		)) (quote ()) (unpack exp)))
+	]]{...}
+	return function(frame)
+		frame[1][key] = mapped(frame)
+		return body(frame)
+	end
+end
 
--- this version is slightly more convenient than above,
--- it falls back on the outer env when reading a missing field.
--- this means you don't have to explicitly capture all the global
--- functions like `print` or `foreach`, etc.
--- with this you can just do `(env foo (print bar))` without any issues.
--- of course, the other version works fine if you wrap your entire
--- code in a big `((fn (print foreach etc) (code)) print foreach etc)`.
--- so it's up to you, really. is it worth the tokens, perf, etc
-def_builtin("env", function(frame, a1, a2)
-	local target, fallback = a1(frame), frame[1][1]
-	local proxy = setmetatable({}, {__index = function(self, field)
-			local res = target[field]
-			if (res == nil) return fallback[field]
-			return res
-		end, __newindex = target})
-	return a2(setmetatable(
-		{setmetatable({proxy}, {__index = frame[1]})},
-		{__index = frame, __newindex = frame}))
-end)
-
--- note: if you aren't particularly strapped for tokens, I'd recommend
---       using field syntax (a.b.c) instead. it'll be faster than setting up
---       all the metatable trickery above for every call.
---       the `[]` builtin is also an option, if a slightly inconvenient one.
---       or, y'know, `rawget` and `rawset` if you *really* need the tokens.
+-- to save you the trouble of capturing all the globals by hand
+-- (capture_api (env foo (print bar)))
+-- this is more or less what pico-8 does internally at launch
+-- in our case, this captures global names *at the parens8[[...]] invocation*
+parens8[[
+(fn (_ENV) (rawset builtin "capture_api" (fn (lookup exp) (
+	(fn (names keys) (select -1
+		(set keys (fn (k) (when k (keys (next _ENV k) (add names k)))))
+		(keys (next _ENV))
+		(compile (pack (pack "fn" names exp) (unpack names)) lookup)
+	))
+	(pack)
+))))
+]](_ENV)
