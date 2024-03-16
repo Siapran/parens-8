e6a907b1509c0402348a5979c45d2394383e16d5
twomice <siapran.candoris@gmail.com>
2024-03-16 16:43:39 -0400

    add repl safe version.

---
+++ experimental/parens8_repl.lua
@@ -0,0 +1,145 @@
+-- parens-8 v3
+-- a lisp interpreter by three rodents
+
+function parens8(code)
+	_pstr, _ppos = "id " .. code .. ")", 0
+	return compile({parse()}, function(name) return name, 1 end){{_ENV}}
+end
+
+function id(...) return ... end
+
+function consume(matches, inv)
+	local start = _ppos
+	while (function()
+		for m in all(matches) do
+			if (_pstr[_ppos] == m) return true
+		end
+	end)() == inv do _ppos += 1 end
+	return sub(_pstr, start, _ppos - 1)
+end
+
+function parse(off)
+	_ppos += off or 1
+	consume(' \n\t', true)
+	local c = _pstr[_ppos]
+	if (not c) return
+	if (c == ';') consume'\n' return parse()  -- comments support
+	if (c == '(') return {parse()}, parse()
+	if (c == ')') return
+	if (c == '"' or c == "'") _ppos += 1 return {"quote", consume(c)}, parse()
+	local token = consume' \n\t()\'"'
+	return tonum(token) or token, parse(0)
+end
+
+builtin = {}
+
+function compile_n(lookup, exp, ...)
+	if (exp) return compile(exp, lookup), compile_n(lookup, ...)
+end
+
+function compile(exp, lookup)
+	if (not exp) return function() end
+	if type(exp) == "string" then
+		local fields, variadic = split(exp, "."), exp == "..."
+		if (fields[2] and not variadic) return fieldview(lookup, deli(fields, 1), fields)
+		local idx, where = lookup(exp)
+		if variadic then
+			return where
+				and function(frame) return unpack(frame[1][where], idx) end
+				or function(frame) return unpack(frame, idx) end
+		end
+		return where
+			and function(frame) return frame[1][where][idx] end
+			or function(frame) return frame[idx] end
+	end
+	if (type(exp) == "number") return function() return exp end
+
+	local op = deli(exp, 1)
+	if (builtin[op]) return builtin[op](lookup, unpack(exp))
+
+	local function ret(s1, ...)
+		local s2 = ... and ret(...)
+		return s2 and function(frame)
+			return s1(frame), s2(frame)
+		end or s1
+	end
+	local fun, args =
+		compile(op, lookup), ret(compile_n(lookup, unpack(exp)))
+	return args and function(frame)
+		return fun(frame)(args(frame))
+	end or function(frame)
+		return fun(frame)()
+	end
+end
+
+function builtin:quote(exp2) return function() return exp2 end end
+
+function builtin:fn(exp1, exp2)
+	local locals, captures, key, close =
+		parens8[[(quote ()) (quote ()) (quote ())]]
+	for i,v in inext, exp1 do locals[v] = i end
+	local body = compile(exp2, function(name)
+		local idx = locals[name]
+		if (idx) return idx + 1, false
+		local idx, where = self(name)
+		if where then captures[where] = true
+		else close = true end
+		return idx, where or key
+	end)
+	return close
+		and function(frame)
+			local upvals = {[key] = frame}
+			for where in next, captures do
+				upvals[where] = frame[1][where]
+			end
+			return function(...)
+				return body{upvals, ...}
+			end
+		end
+		or function(frame)
+			return function(...)
+				return body{frame[1], ...}
+			end
+		end
+end
+
+parens8[[
+(fn (closure) (rawset builtin "when" (fn (lookup e1 e2 e3)
+	(closure (compile_n lookup e1 e2 e3))
+)))
+]](function(a1, a2, a3) return
+	function(frame)
+		if (a1(frame)) return a2(frame)
+		if (a3) return a3(frame)
+	end
+end)
+
+parens8[[
+(fn (closures) (rawset builtin "set" (fn (lookup exp1 exp2)
+	((fn (compiled fields) ((fn (head tail) (when tail
+		(select 3 (closures compiled tail (fieldview lookup head fields)))
+		((fn (idx where) (select (when where 1 2)
+			(closures compiled idx where)
+		)) (lookup head))
+	)) (deli fields 1) (deli fields))) (compile exp2 lookup) (split exp1 "."))
+)))
+]](function(compiled, idx, where) return
+	function(frame) frame[1][where][idx] = compiled(frame) end,
+	function(frame) frame[idx] = compiled(frame) end,
+	function(frame) where(frame)[idx] = compiled(frame) end
+end)
+
+parens8[[
+(fn (closure) (set fieldview (fn (lookup tab fields view) (select -1
+	(set view (fn (step i field) (when field (view
+		(closure step field)
+		(inext fields i)
+	) step)))
+	(view (compile tab lookup) (inext fields))
+))))
+]](function(step, field)
+	return function(frame)
+		return step(frame)[field]
+	end
+end)
+
