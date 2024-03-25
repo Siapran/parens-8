f7586ce0d0bbfa2201b4e20e5711b6553e23d3d6
twomice <siapran.candoris@gmail.com>
2024-03-25 00:41:15 -0400

    expand rom utils.

--- rom-utils/writerom.lua
+++ rom-utils/writerom.lua
@@ -1,5 +1,6 @@
 function writerom(str, addr, filename)
 	local target = filename and 0x8000 or addr
+	assert(addr + #str > addr, "string does not fit in cart ROM")
 	poke(target, ord(str, 1, #str))
 	cstore(addr, target, #str, filename)
 	return #str
@@ -15,7 +16,13 @@ function write_many(addr, filename, ...)
 		acc ..= "))\n"
 		return iterate(acc, a + len, ...)
 	end
-	local res = iterate("parens8[[\n", addr, ...) .."]]"
-	print(res)
-	printh(res, "@clip")
+	return iterate("parens8[[\n", addr, ...) .."]]"
+end
+
+function write_clipboard(addr, filename, ...)
+	printh(write_many(addr, filename, ...), "@clip")
+end
+
+function write_module(module_header, module_data, ...)
+	printh(write_many(0, module_data, ...), module_header, true)
 end
