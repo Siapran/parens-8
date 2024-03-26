7692498690a0ff9e71f2b3c5988bce86395abb2b
twomice <siapran.candoris@gmail.com>
2024-03-26 00:14:34 -0400

    improve write_module.

--- rom-utils/writerom.lua
+++ rom-utils/writerom.lua
@@ -1,6 +1,8 @@
 function writerom(str, addr, filename)
+	assert(
+		addr>>16 + #str>>16 < 0x0.8000,
+		"string does not fit in cart ROM")
 	local target = filename and 0x8000 or addr
-	assert(addr + #str > addr, "string does not fit in cart ROM")
 	poke(target, ord(str, 1, #str))
 	cstore(addr, target, #str, filename)
 	return #str
@@ -10,20 +12,26 @@ end
 function write_many(addr, filename, ...)
 	local function hex(num) return sub(tostr(num, 1), 1, 6) end
 	local function iterate(acc, a, head, ...)
-		if (not head) return acc
+		if (not head) return acc, a
 		local len = writerom(minify(head), a, filename)
 		acc ..= "(parens8 (readrom " .. hex(a) .. " " .. hex(len)
 		if (filename) acc ..= ' "' .. filename .. '"'
 		acc ..= "))\n"
 		return iterate(acc, a + len, ...)
 	end
-	return iterate("parens8[[\n", addr, ...) .."]]"
+	local res, len = iterate("parens8[[\n", addr, ...)
+	print("wrote " .. len .. " (" .. hex(len) .. ") bytes")
+	return res .."]]"
 end
 
 function write_clipboard(addr, filename, ...)
-	printh(write_many(addr, filename, ...), "@clip")
+	local str, len = write_many(addr, filename, ...)
+	printh(str, "@clip")
+	return len
 end
 
 function write_module(module_header, addr, module_data, ...)
-	printh(write_many(addr, module_data, ...), module_header, true)
+	local str, len = write_many(addr, module_data, ...)
+	printh(str, module_header, true)
+	return len
 end
