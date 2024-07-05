ac7c0629d1a72f8465592fd8b0f434caf86d5c68
Katherine Miller <katherine.miller@libertyrobotics.com>
2024-07-05 13:45:15 -0400

    Implement compound assignment operators.

--- v3/builtin/operators.lua
+++ v3/builtin/operators.lua
@@ -1,5 +1,5 @@
 parens8[[
-(fn (opstr closures) ((fn (ops loopfn) (select -1
+(fn (closures) ((fn (ops loopfn) (select -1
 	(set loopfn (fn (i op) (when i (loopfn (select 2
 		(rawset builtin op (fn (lookup e1 e2 e3)
 			(select i (closures (compile_n lookup e1 e2 e3)))
@@ -7,9 +7,8 @@ parens8[[
 		(inext ops i)
 	)))))
 	(loopfn (inext ops))
-)) (split opstr)))
-]]("+,-,*,/,\\,%,^,<,>,==,~=,..,or,and,not,#,[]",
-function(a1, a2, a3) return
+)) (split "+,-,*,/,\,%,^,<,>,==,~=,..,or,and,not,#,[]")))
+]](function(a1, a2, a3) return
 	function(f) return a1(f)+a2(f) end,
 	a2 and function(f) return a1(f)-a2(f) end
 		or function(f) return -a1(f) end,
@@ -30,3 +29,11 @@ function(a1, a2, a3) return
 	a3 and function(f) a1(f)[a2(f)] = a3(f) end
 		or function(f) return a1(f)[a2(f)] end
 end)
+
+parens8[[
+(foreach (split "+,-,*,/,\,%,^,..") (fn (op)
+	(rawset builtin (.. op "=") (fn (lookup e1 e2)
+		(compile (pack "set" e1 (pack op e1 e2)) lookup)
+	))
+))
+]]
